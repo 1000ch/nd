aef23fcfd2d543f35dd31c7f08d7448c8f5d5e69
1000ch <shogo.sensui@gmail.com>
2018-10-14 14:37:36 +0900

    Create symlink on global

--- global.go
+++ global.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"os"
+	"path/filepath"
 
 	"github.com/google/subcommands"
 )
@@ -12,7 +14,7 @@ type globalCommand struct {
 }
 
 func (*globalCommand) Name() string {
-	return "gloabl"
+	return "global"
 }
 
 func (*globalCommand) Synopsis() string {
@@ -28,6 +30,35 @@ func (i *globalCommand) SetFlags(f *flag.FlagSet) {}
 func (i *globalCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	err := initialize()
 	if err != nil {
+		return subcommands.ExitFailure
+	}
+
+	args := f.Args()
+	if len(args) != 1 {
+		return subcommands.ExitFailure
+	}
+
+	binPath, err := getBinaryDir()
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+	if err := prepareDir(binPath); err != nil {
+		return subcommands.ExitSuccess
+	}
+
+	versionsDir, err := getVersionsDir()
+	if err != nil {
+		return subcommands.ExitSuccess
+	}
+
+	symlinkTarget := filepath.Join(versionsDir, normalizeVersion(args[0]), "bin/node")
+	symlinkPath := filepath.Join(binPath, "node")
+
+	if err := os.Remove(symlinkPath); err != nil {
+		return subcommands.ExitFailure
+	}
+
+	if err := os.Symlink(symlinkTarget, symlinkPath); err != nil {
 		return subcommands.ExitFailure
 	}
 
